d847cb51e3d537ceb0a11e8057e94dcc13a39075
nao <chip080415@gmail.com>
2020-10-30 14:27:35 +0900

    LEDとポート定義の変更

--- list01/list01.ino
+++ list01/list01.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 #define SW 5    /* タクトスイッチのポート番号 */
+#define LED 10
 
 Servo mServo1;
 Servo mServo2;
@@ -11,21 +12,23 @@ int theta1,theta2,theta3;
 void setup()
 {
     /* サーボモータのI/Oポートを入力する */
-    mServo1.attach(8);
-    mServo1.attach(6);
-    mServo1.attach(3);
+    mServo1.attach(3);  /* 第1関節サーボモーター */
+    mServo1.attach(6);  /* 第2関節サーボモーター */
+    mServo1.attach(9);  /* 第3関節サーボモーター */
     pinMode(SW,INPUT);
+    pinMode(LED,OUTPUT);
     Serial.begin(9600);
 }
 
 void loop(){
     int flag = 0;
 
+    digitalWrite(LED,OUTPUT);
     while(1){
         /* ボリュームのポート番号を設定する */     
-        ad0 = analogRead(0);
-        ad1 = analogRead(1);
-        ad2 = analogRead(2);
+        ad0 = analogRead(0);    /* ボリュームセンサ1 */
+        ad1 = analogRead(1);    /* ボリュームセンサ2 */
+        ad2 = analogRead(2);    /* ボリュームセンサ3 */
 
         theta1 = ( ad0 * 180 / 1024);
         theta2 = ( ad1 * 180 / 1024);
@@ -52,6 +55,7 @@ void loop(){
 
     while(1){
         /* 無限ループ：処理終了 */
+        digitalWrite(LED,LOW);
     }
     
 }
